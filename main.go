8c1ac374f217f559498c2e3d0018bccca3ae2e7d
Beatriz Rodrigues <beatrizrdgs@users.noreply.github.com>
2024-06-06 21:14:03 -0300

    feat: get cmd output and print multi value helper funcs

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ import (
 	"github.com/shirou/gopsutil/mem"
 )
 
+const (
+	UNKNOWN = "unknown"
+)
+
 func main() {
 	sys := NewSystem()
 	sys.printAttributes()
@@ -33,7 +37,7 @@ type System struct {
 func NewSystem() *System {
 	hostname, err := os.Hostname()
 	if err != nil {
-		hostname = ""
+		hostname = UNKNOWN
 	}
 
 	return &System{
@@ -59,56 +63,48 @@ func (s *System) printAttributes() {
 	fmt.Println("Distro:", s.Distro)
 	fmt.Println("Kernel:", s.Kernel)
 	fmt.Println("Shell:", s.Shell)
-	fmt.Println("CPU:", s.CPU)
-	fmt.Println("GPU:", s.GPU)
+	printMultiValues("CPU", s.CPU)
+	printMultiValues("GPU", s.GPU)
 	fmt.Println("Memory:", s.Disk, "MiB /", s.RAM, "MiB")
 }
 
-func getHost() string {
-	cmd := exec.Command("cat", "/sys/devices/virtual/dmi/id/product_name")
-	out, err := cmd.Output()
-	if err != nil {
-		return ""
+func printMultiValues(item, val string) {
+	if strings.Contains(val, ", ") {
+		ss := strings.Split(val, ", ")
+		for _, s := range ss {
+			fmt.Println(item+":", s)
+		}
+	} else {
+		fmt.Println(item+":", val)
 	}
+}
 
-	return string(out[:len(out)-1])
+func getHost() string {
+	out := getCmdOutput("cat", "/sys/devices/virtual/dmi/id/product_name")
+	return out[:len(out)-1]
 }
 
 func getGoVersion() string {
-	cmd := exec.Command("go", "version")
-	out, err := cmd.Output()
-	if err != nil {
-		return ""
-	}
-
-	return string(out[13:19])
+	out := getCmdOutput("go", "version")
+	return out[13:19]
 }
 
 func getDistro() string {
-	cmd := exec.Command("cat", "/etc/os-release")
-	out, err := cmd.Output()
-	if err != nil {
-		return ""
-	}
-	start := strings.Index(string(out), "NAME=") + len("NAME=") + 1
-	end := strings.Index(string(out), "\"\n")
-	return string(out)[start:end]
+	out := getCmdOutput("cat", "/etc/os-release")
+	start := strings.Index(out, "NAME=") + len("NAME=") + 1
+	end := strings.Index(out, "\"\n")
+	return out[start:end]
 }
 
 func getKernel() string {
-	cmd := exec.Command("uname", "-r")
-	out, err := cmd.Output()
-	if err != nil {
-		return ""
-	}
-
-	return string(out[:len(out)-1])
+	out := getCmdOutput("uname", "-r")
+	return out[:len(out)-1]
 }
 
 func getCPU() string {
 	cpuInfo, err := cpu.Info()
 	if err != nil {
-		return ""
+		return UNKNOWN
 	}
 
 	var cpus []string
@@ -124,28 +120,25 @@ func getCPU() string {
 }
 
 func getGPU() string {
-	cmd := exec.Command("lspci")
-	output, err := cmd.Output()
-	if err != nil {
-		return ""
-	}
+	out := getCmdOutput("lspci")
 
-	lines := strings.Split(string(output), "\n")
+	var gpus []string
+	lines := strings.Split(out, "\n")
 	for _, line := range lines {
 		if strings.Contains(line, "VGA compatible controller") || strings.Contains(line, "3D controller") {
 			start := strings.Index(line, "controller") + len("controller: ")
 			end := strings.Index(line, " (rev")
-			return line[start:end]
+			gpus = append(gpus, line[start:end])
 		}
 	}
 
-	return ""
+	return strings.Join(gpus, ", ")
 }
 
 func getRAMUsage() string {
 	vmStat, err := mem.VirtualMemory()
 	if err != nil {
-		return ""
+		return UNKNOWN
 	}
 	ram := vmStat.Total / 1024 / 1024
 	return fmt.Sprint(ram)
@@ -154,10 +147,19 @@ func getRAMUsage() string {
 func getDiskUsage() string {
 	diskStat, err := disk.Usage("/")
 	if err != nil {
-		return ""
+		return UNKNOWN
 	}
 	disk := diskStat.Total / 1024 / 1024
 	return fmt.Sprint(disk)
+}
+
+func getCmdOutput(name string, arg ...string) string {
+	cmd := exec.Command(name, arg...)
+	out, err := cmd.Output()
+	if err != nil {
+		return UNKNOWN
+	}
+	return string(out)
 }
 
 var gopherASCII = `
